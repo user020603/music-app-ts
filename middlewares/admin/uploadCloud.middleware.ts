add109c09e72a76a01de7704bb68787fb541f85a
user020603 <ptit.tienthanh@gmail.com>
2024-03-25 13:13:03 +0700

    upload audio

--- middlewares/admin/uploadCloud.middleware.ts
+++ middlewares/admin/uploadCloud.middleware.ts
@@ -41,3 +41,25 @@ export const uploadSingle = async (req: Request, res: Response, next: NextFuncti
 
   next();
 };
+
+export const uploadFields = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    for (const key in req["files"]) {
+      req.body[key] = [];
+
+      const array = req["files"][key];
+      for (const item of array) {
+        try {
+          const result = await uploadToCloudinary(item.buffer);
+          req.body[key].push(result);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  next();
+};
